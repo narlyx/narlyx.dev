94a1a495505f106ff8e87ce20feae0d716903068
Narlyx <109835029+narlyx@users.noreply.github.com>
2025-04-08 15:25:31 -0600

    Feat: New quotes + copywrite

--- src/scripts/randomPhrases.js
+++ src/scripts/randomPhrases.js
@@ -1,69 +1,26 @@
 // List of random phrases
 const generalPhrases = [
-  "HELP ME",
-  "Rah",
-  "*Insert Random Text*",
-  "B_unno was here",
-  "It's your birthday!",
-  "UAGH",
-  "Mood af",
-  "Hourse",
-  "hElp asnurm",
-  "I'm loosing it.",
-  "I'm gonna freak.",
-  "This is what real life feels like",
-  "I CAN ONLY COUNT TO FOOOOAAAARRRR!",
-  "I sit... in my desolate room...",
-  "No lights... no music...",
-  "JUST ANGERR!",
-  "GIMMI THAT SOCK, OM NOM NOM!",
-  "Maw",
-  "PAST THE POINT OF DELIRIUM",
-  "My everything hurts.",
-  "Are weee still friends",
-  "STAY IN DONT HELP YOURSELFF",
-  "I use arch btw",
-  "Vim > Emacs",
-  "Relax, I don't got this.",
-  "Why can't we be frienndss",
-  "I hate javascript.",
-  "React is for loosers",
-  "Do people even read these?",
-  "Space is pretty cool",
-  "What's Unus Annus?",
-  "A glorified linktree",
-  "How to javascript?",
-  "Turtles :(",
-  "Wusyaname girllfriendd",
+  "ARE WE STILL FRIENDS?...",
+  "THEY'RE GETTING TIRED OF ME BEING SAD",
+  "You make me feel like a foooooollll...",
   "Damn.",
-  "I think im falllinngg in looveeee...",
-  "This time I think its for reaallllll...",
-  "Scotty doesn't know",
-  "I was all over her",
-  "Harness your hopes on just one person",
-  "You could have anyone you want...",
+  "Relax, I don't got this!",
+  "DON'T GET GREEN SKIN, KEEP CONTACT!",
+  "Time heals but I have no time.",
+  "I was in hell... looking at heaven.",
+  "You were the right oneeee...",
+  "Maybe we meet again in another lifetime.",
+  "When I was here, I wanted to be there.",
   "Why would you want to be with me?",
-  "I LOVE YOU SO",
-  "Meee anndd the biirddssss",
-  "You were the right one",
-  "YOU AND ME, ALWAYS, FOREVER",
-  "IGOR",
-  "I hope you answer...",
-  "I HATE YOU THOUUUGHHH",
-  "MISTAKES ON SOMEONEEE",
-  "GETTIN' TIRED OF BEING HAD",
-  "THEY'RE GETTIGN TIRED OF ME BEING SAD",
-  "Push your t3mpr",
-  "I GOT CELLOPHANE WRAPPED AROUND THE LINING OF MY THROAT",
-  "Roll the Katamari",
-  "DOGMATICA",
-  "SNAP OUT OF IT",
-  "Teddy Picker",
-  "You make me feel like a fooool",
-  "I don't practice Santeria!",
+  "YOU AND MEEE ALWAYYYSS FOREVERRRR",
+  "MISTAKES ON SOMEONEEEE",
   "FE!N FE!N FE!N",
-  "To me you mean the most babbbyyyy...",
-  "YOU ALLLREADYYY KNOOWWWW!",
+  "Past the point of delerium.",
+  "Carry meeee further than regret",
+  "Tootles.",
+  "Die young and leave a pretty corpse.",
+  "The loneliness came back... worse that I remebered...",
+  "I'm the dream catcher but nothing but nightmares I've caught.",
 ];
 
 const octoberPhrases = [
@@ -78,7 +35,6 @@ const octoberPhrases = [
   "Halloween",
   "Rahhhhh",
   "Roarrr",
-  "I'M NOT A FURRY, IS HALLOWEEN!"
 ];
 
 // Getting current date and storing it
@@ -86,20 +42,15 @@ var day;
 var month;
 
 day = new Date().getDay();
-month = new Date().getMonth() +1;
+month = new Date().getMonth() + 1;
 
 // Returns randomized phrases
 function getPhrase() {
-  var phrase;
-   
   if (month == 10) {
-    phrase = octoberPhrases[Math.floor(Math.random() * octoberPhrases.length)];
+    return octoberPhrases[Math.floor(Math.random() * octoberPhrases.length)];
   }
-  else {
-    phrase = generalPhrases[Math.floor(Math.random() * generalPhrases.length)];
-  }
-  
-  return phrase;
+
+  return generalPhrases[Math.floor(Math.random() * generalPhrases.length)];
 }
 
 // Setting a global random phrase
@@ -115,8 +66,14 @@ async function titleLoad() {
 async function splashLoad() {
   document.getElementById("splash");
   await new Promise((resolve) => setTimeout(resolve, 350));
-  document.getElementById("splash").innerText = globalPhrase;
+  document.getElementById("splash").innerText = '"' + globalPhrase + '"';
+}
+async function quoteLoad() {
+  document.getElementById("quote");
+  await new Promise((resolve) => setTimeout(resolve, 350));
+  document.getElementById("quote").innerText = '"' + globalPhrase + '"';
 }
 
 titleLoad();
 splashLoad();
+quoteLoad();
